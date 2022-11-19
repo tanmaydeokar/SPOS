2d1a33e524ce0bab8f8737032e064d1dcd772c50
Tanmay Deokar <100110322+tanmaydeokar@users.noreply.github.com>
2022-11-19 21:30:53 +0530

    Update fcfs.java

--- fcfs.java
+++ fcfs.java
@@ -1,49 +1,69 @@
-import java.io.*;
 import java.util.Scanner;
-public class fcfs
-{
-	public static void main(String args[])
-	{
-		int i,no_p,burst_time[],TT[],WT[];
-		float avg_wait=0,avg_TT=0;
-		burst_time=new int[50];
-		TT=new int[50];
-		WT=new int[50];
-		WT[0]=0;
-		Scanner s=new Scanner(System.in);
-		System.out.println("Enter the number of process: ");
-		no_p=s.nextInt();
-		System.out.println("\nEnter Burst Time for processes:");
-		for(i=0;i<no_p;i++)
-		{
-			System.out.print("\tP"+(i+1)+":  ");
-			burst_time[i]=s.nextInt();
+public class fcfs{
+	public static void main(String args[]){
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the number of the process:");
+		int n=sc.nextInt();
+		int pid[]=new int[n];
+		int ar[]=new int[n];
+		int bt[]=new int[n];
+		int ct[]=new int[n];
+		int wt[]=new int[n];
+		int ta[]=new int[n];
+		int avgta=0,avgwt=0,temp;
+		for(int i=0;i<n;i++){
+			System.out.println("Enter the arrival time of the process: "+(i+1)+":");
+			ar[i]=sc.nextInt();
+			System.out.println("Enter the burst time of the process:"+(i+1)+":");
+			bt[i]=sc.nextInt();
+			pid[i]=i+1;
 		}
+		for(int i=0;i<n;i++){
+			for(int j=0;j<(n-(i+1));j++){
+				if(ar[j]>ar[j+1]){
+					temp=ar[i];
+					ar[i]=ar[i+1];
+					ar[i+1]=temp;
+
+					temp=pid[i];
+					pid[i]=pid[i+1];
+					pid[i+1]=temp;
+
+					temp=bt[i];
+					bt[i]=bt[i+1];
+					bt[i+1]=temp;
+
+					
+
+				}
+			}
+		}for(int i=0;i<n;i++){
+			if(i==0){
+				ct[i]=ar[i]+bt[i];
+
+			}else{
+				if(ar[i]>ct[i-1]){
+					ct[i]=ar[i]+bt[i];
+				}
+				else{
+					ct[i]=ct[i-1]+bt[i];
+				}
+			}
+			ta[i]=ct[i]-ar[i];
+			wt[i]=ta[i]-bt[i];
+			avgta+=ta[i];
+			avgwt+=wt[i];
+		}
+		System.out.println("PNo \t AT \t   BT  \t Ct \t WT \t TT");
+		for(int i=0;i<n;i++){
 		
-		for(i=1;i<no_p;i++)
-		{
-			WT[i]=WT[i-1]+burst_time[i-1];
-			avg_wait+=WT[i];
+			System.out.println(pid[i]+"\t"+ ar[i]+"\t" +bt[i]+"\t"+ct[i]+"\t"+wt[i]+"\t"+ta[i]);
 		}
-		avg_wait/=no_p;
-		
-		for(i=0;i<no_p;i++)
-		{
-			TT[i]=WT[i]+burst_time[i];
-			avg_TT+=TT[i];
-		}
-		avg_TT/=no_p;
-		System.out.println("\n****************************************************************");
-		System.out.println("\tProcesses:");
-		System.out.println("****************************************************************");
-		System.out.println("    Process\tBurst Time\tWaiting Time\tTurn Around Time");
-		for(i=0;i<no_p;i++)
-		{
-			System.out.println("\tP"+(i+1)+"\t  "+burst_time[i]+"\t\t  "+WT[i]+"\t\t "+TT[i]);
-			
-		}
-		System.out.println("\n----------------------------------------------------------------");
-		System.out.println("\nAverage waiting time : "+avg_wait);
-		System.out.println("\nAverage Turn Around time : "+avg_TT+"\n");
+		sc.close();
+		System.out.println("Average Waiting time is"+(avgwt/n));
+		System.out.println("Average Turnaround time is"+(avgta/n));
 	}
 }
+
+
+
